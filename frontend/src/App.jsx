4099a101feab3a49e3a4d5da8f3fee99827d6cbf
m-aaraiz <aaraizmo@gmail.com>
2024-03-06 20:46:40 -0700

    initial vite set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<main>
+			<h1>StudySprint</h1>
+
+			{/* Need to split into components */}
+			<div className="main-box">
+				<div className="timer-card">Timer card</div>
+				<div className="tasks-card">
+					Tasks card
+					<input type="text" placeholder="Enter task" />
+				</div>
+				<div className="goals-card">Goals card</div>
+			</div>
+
+			<Footer />
+		</main>
+	);
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'; // Importing a CSS file to style the footer
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <button className="footer-button">Button 1</button>
+            <button className="footer-button">Button 2</button>
+            <button className="footer-button">Button 3</button>
+        </div>
+    );
+}
+
+export default Footer;
